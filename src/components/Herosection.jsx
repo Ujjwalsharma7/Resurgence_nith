328c2ddfd1a8f4ad35957d9649ebdad73b7de01d
Shryansh <shryansh2024@gmail.com>
2023-09-09 19:37:33 +0530

    added a svg background

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { Typography } from "@material-tailwind/react";
 import Socials from "./SocialMediaIcons";
-import bg from '../assets/images/new.svg'
+import bg from "../assets/images/bg.svg";
 import NavbarDefault from "../components/Navbar";
 
 
 const HeroSection = () => {
     return (
         <>
-            <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
-                <div><NavbarDefault/>
+            <div className=" grid grid-cols-1 gap-2 lg:grid-cols-2">
+                <div>
+                    <img
+                        src={bg}
+                        alt="background"
+                        className="absolute z-[-1]  w-full h-full object-cover"
+                    />
+                    <NavbarDefault />
                     <Typography className="text-[5.3rem] pt-[16rem] text-[#eaecec] custom-font lg:text-[10rem]">
                         Resurgence
                     </Typography>
                     <Typography className="custom-font text-[2.3rem] pt-[5rem] pb-[18rem]">
-                        For Vibes and Games
+                    For Vibes and Games
                     </Typography>
-                    <Socials/>
+                    
+                    <Socials />
                 </div>
-                <div className="w-full h-full">
-                    <img src={bg} alt="background" className="w-full h-full object-cover"/>
-                </div>
+                <div className="w-full h-full"></div>
             </div>
-            
         </>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,33 +20,18 @@ const NavbarDefault = () => {
     }, []);
 
     const navList = (
-        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-            <Typography
-                as="li"
-                variant="small"
-                color="white"
-                className="p-1 font-normal"
-            >
-                <Link to="/" className="flex items-center">
+        <ul className=" mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-12">
+            <Typography as="li" color="white" className="p-1 ">
+                <Link to="/" className="flex items-center ">
                     Home
                 </Link>
             </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color=""
-                className="p-1 font-normal"
-            >
+            <Typography as="li" color="" className="p-1 ">
                 <Link to="/team" className="flex items-center">
                     Team
                 </Link>
             </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="white"
-                className="p-1 font-normal"
-            >
+            <Typography as="li" color="white" className="p-1 2">
                 <Link to="/events" className="flex items-center">
                     Events
                 </Link>
@@ -57,7 +42,7 @@ const NavbarDefault = () => {
     return (
         <Navbar
             color="blue-gray"
-            className="bg-[#171717] mx-auto border-none py-2 px-4 lg:px-8 lg:py-4"
+            className="bg-[#131212] mx-auto border-none py-2 px-4 lg:px-8 lg:py-4"
         >
             <div className="container mx-auto flex items-center justify-between text-white">
                 <Avatar src={image} alt="avatar" />
